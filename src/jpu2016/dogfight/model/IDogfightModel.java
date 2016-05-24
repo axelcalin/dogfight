a32360eb9b8d8d2ace6fb9cda084adaee118a01e
Maxime Poitevin <maxime.poitevin@viacesi.fr>
2016-05-24 14:28:31 +0200

    ma bite!

--- src/jpu2016/dogfight/model/IDogfightModel.java
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -1,5 +1,11 @@
 package jpu2016.dogfight.model;
 
-public class IDogfightModel {
+import java.util.ArrayList;
 
+public interface IDogfightModel {
+	public IArea getArea();
+	public void buildArea(Dimension);
+	public void addMobile(IMobile);
+	public void removeMobile(IMobile);
+	public ArrayList<IMobile> getMobile();
 }
