157b3e4bce8c02e8d0809e16664dc3f928dc12c8
Maxime Poitevin <maxime.poitevin@viacesi.fr>
2016-05-24 14:37:58 +0200

    penis

--- src/jpu2016/dogfight/model/IDogfightModel.java
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -1,12 +1,13 @@
 package jpu2016.dogfight.model;
 
 import java.util.ArrayList;
-import jpu2016.dogfight.model;
 
 public interface IDogfightModel {
 	public IArea getArea();
-	public void buildArea(Dimension);
-	public void addMobile(IMobile);
-	public void removeMobile(IMobile);
+	public void buildArea(Dimension dimension);
+	public void addMobile(IMobile Mobile);
+	public void removeMobile(IMobile Mobile);
 	public ArrayList<IMobile> getMobile();
+	public IMobile getMobilesByPlayer(int player);
+	public void setMobilesHaveMoved();
 }
